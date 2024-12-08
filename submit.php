41298356739f1e6426a0ec83833263b318fc97ed
shalinimc <shalinimc2004@gmail.com>
2024-12-08 21:35:23 +0530

    Create submit.php

---
+++ submit.php
@@ -0,0 +1,23 @@
+<?php echo "
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<meta charset='UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<title>Submission Successful</title>
+<link rel='stylesheet' href='style.css'>
+</head>
+<body>
+<div class='form-container'>
+<h2>Submission Successful!</h2>
+<p><strong>Name:</strong> Shalini M C</p>
+<p><strong>Email:</strong> shalinimc2004@gmail.com</p>
+ 
+<p><strong>Phone:</strong> 1234567890</p>
+<p><strong>Date of Birth:</strong> 28-11-2004</p>
+<p><strong>Address:</strong> Chikkamagaluru</p>
+<a href='index.html'>Go back to the form</a>
+</div>
+</body>
+</html>
+
